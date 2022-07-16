d28efc63cf42213375ea68211fcee247effc8cae
szh <songzihui@bilibili.com>
2022-07-17 00:32:27 +0800

    Revert "0-1Package, TargetSum_494方法二: 转化为SunSetSum问题"
    This reverts commit 5709143137d261229bfd520e5368111288a35f3a.

--- src/DP/ZeroOnePackage/TargetSum_494.java
+++ src/DP/ZeroOnePackage/TargetSum_494.java
@@ -1,13 +1,7 @@
 package DP.ZeroOnePackage;
 
 /*
-    思路一: 正常0-1背包
-        dp[i][j + sum] = dp[i - 1][j + sum - nums[i]] + dp[i - 1][j + sum + nums[i]]
-
-    思路二: 转化为Subset Sum问题, 所有取正号或者是负号的元素和等于target, 然后用0-1背包求解
-                          sum(P) - sum(N) = target
-            sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
-                       sum(P) = (target + sum(nums)) / 2
+    dp[i][j + sum] = dp[i - 1][j + sum - nums[i]] + dp[i - 1][j + sum + nums[i]]
  */
 
 public class TargetSum_494 {
@@ -15,27 +9,6 @@ public class TargetSum_494 {
         int[] nums = {1, 1, 1, 1, 1};
         int target = 3;
         System.out.println(findTargetSumWays_1(nums, target));
-        System.out.println(findTargetSumWays_2(nums, target));
-    }
-
-    private static int findTargetSumWays_2(int[] nums, int target) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (Math.abs(target) > sum || (sum + target) % 2 != 0) {
-            return 0;
-        }
-        int range = (sum + target) / 2;
-        int[] dp = new int[range + 1];
-        dp[0] = 1;
-        for (int num : nums) {
-            for (int j = range; j >= num; j--) {
-                dp[j] = dp[j] + dp[j - num];
-            }
-        }
-
-        return dp[range];
     }
 
     public static int findTargetSumWays_1(int[] nums, int target) {
