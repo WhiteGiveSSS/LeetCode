a6d4cbd3c197d2cf5deac0cdd2220a5f3eec120f
songzihui <songzihuihuihui@gmail.com>
2022-08-20 11:54:56 +0800

    Hot100: 括号生成, 下一个排列, 旋转数组二分查找

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] nums = {1, 3, 2, 0};
+        Arrays.sort(nums, 1, 4);
+        System.out.println(Arrays.toString(nums));
     }
 }
