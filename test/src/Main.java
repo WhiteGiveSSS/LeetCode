2bfe0907ae7f9e37c3a824a15316de4442460960
songzihui <songzihuihuihui@gmail.com>
2022-08-31 12:58:53 +0800

    Hot100: LRU

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,27 +1,10 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<>();
-
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Integer integer = map.get(1);
+        System.out.println(integer);
     }
 
-    public boolean check(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
-            return true;
-        }
-        if ((left == null && right != null) || (left != null && right == null)) {
-            return false;
-        }
-        if (left.val != right.val) {
-            return false;
-        }
-        if (left.left.val == right.right.val && left.right.val == right.left.val) {
-            return check(left.left, right.right) && check(left.right, right.left);
-        }
-        return false;
-    }
 }
