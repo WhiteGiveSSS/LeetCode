35ecf09f6fbfecfc719548a661763764e383f68e
songzihui <songzihuihuihui@gmail.com>
2022-09-20 13:24:14 +0800

    Hot100: 字符串同分异构体

--- test/src/Main.java
+++ test/src/Main.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("q"+10);
+        int[] a = new int[]{0, 1};
+        int[] b = new int[]{1, 0};
+        System.out.println(Arrays.equals(a, b));
     }
 }
 
